60bb01b05b5316c85f79a453cab915423eac3219
sharipovme <sharipovsdev@gmail.com>
2024-03-27 15:30:25 +0500

    env file

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,5 +1,5 @@
 const settings = {
-  baseURL: "https://face.taqsim.uz/api",
+  baseURL: process.env.REACT_APP_BASE_URL,
   requestTimeout: 60000 * 5,
   rowsPerPage: 10,
   staleTime: 1000 * 60 * 2,
