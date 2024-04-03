1117900b22075569cbd6e7e47c841a219ceb6c32
sharipovme <sharipovsdev@gmail.com>
2024-04-03 16:56:35 +0500

    online table updated

--- src/jsx/components/pages/online-table/online-table.jsx
+++ src/jsx/components/pages/online-table/online-table.jsx
@@ -3,6 +3,7 @@ import { getAttendancesQuery } from "../../../../queries/index";
 import { useEffect, useState } from "react";
 import { getAttendancesDetail } from "../../../../api";
 import settings from "../../../../settings/settings";
+import { useTranslation } from "react-i18next";
 
 const getCurrentDate = () => {
   var today = new Date();
@@ -24,6 +25,7 @@ export function OnlineTable() {
   const { data: attendances } = useQuery({
     ...getAttendancesQuery(getCurrentDate()),
   });
+  const { t } = useTranslation();
 
   useEffect(() => {
     if (selectedClass) {
@@ -35,7 +37,7 @@ export function OnlineTable() {
 
   return (
     <div>
-      <div className="row px-3">
+      <div className="row px-3" style={{ gap: "24px" }}>
         {attendances?.result.map((item, i) => (
           <div
             className="card col-xl-3"
@@ -64,55 +66,116 @@ export function OnlineTable() {
       </div>
       {selectedClass && (
         <div className="col-xl-12 card wow fadeInUp" data-wow-delay="1.5s">
-          <div className="table-responsive full-data">
-            <div
-              id="example-student_wrapper"
-              className="dataTables_wrapper no-footer"
-            >
-              <table
-                style={{ paddingBottom: "100px" }}
-                className="table-responsive-lg table display dataTablesCard student-tab dataTable no-footer"
-                id="example-student"
+          <div className="card">
+            <div className="card-header pb-0 border-0 flex-wrap">
+              <div>
+                <div className="mb-3">
+                  <h2 className="heading mb-0" style={{ color: "red" }}>
+                    {t("not_attended")}
+                  </h2>
+                </div>
+              </div>
+            </div>
+            <div className="table-responsive full-data">
+              <div
+                id="example-student_wrapper"
+                className="dataTables_wrapper no-footer"
               >
-                <thead>
-                  <tr>
-                    <th>Image</th>
-                    <th>Name</th>
-                    <th>Date of birth</th>
-                    <th>Phone number</th>
-                    <th>Class name</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {students?.result?.map((item, ind) => (
-                    <tr key={ind}>
-                      <td>
-                        <img
-                          src={`${settings.baseURL}/images?filename=${item.mainImageName}`}
-                          alt=""
-                          style={{ width: "50px", borderRadius: "8px" }}
-                        />
-                      </td>
-                      <td>
-                        <div className="trans-list">
-                          <h4>{`${item.firstName} ${item.lastName}`}</h4>
-                        </div>
-                      </td>
-                      <td>
-                        <h6 className="mb-0">
-                          {item.dateOfBirth.slice(0, 10)}
-                        </h6>
-                      </td>
-                      <td>
-                        <h6 className="mb-0">{item.phoneNumber}</h6>
-                      </td>
-                      <td>
-                        <h6 className="mb-0">{item.className}</h6>
-                      </td>
+                <table
+                  className="table-responsive-lg table display dataTablesCard student-tab dataTable no-footer"
+                  id="example-student"
+                >
+                  <thead>
+                    <tr>
+                      <th>{t("image")}</th>
+                      <th>{t("name")}</th>
+                      <th>{t("phoneNumber")}</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {students?.result?.map(
+                      (item, ind) =>
+                        !item?.attended && (
+                          <tr key={ind}>
+                            <td>
+                              <img
+                                src={`${settings.baseURL}/images?filename=${item.mainImageName}`}
+                                alt=""
+                                style={{ width: "50px", borderRadius: "8px" }}
+                              />
+                            </td>
+                            <td>
+                              <div className="trans-list">
+                                <h4>{`${item.firstName} ${item.lastName}`}</h4>
+                              </div>
+                            </td>
+                            <td>
+                              <h6 className="mb-0">{item.phoneNumber}</h6>
+                            </td>
+                          </tr>
+                        )
+                    )}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {selectedClass && (
+        <div className="col-xl-12 card wow fadeInUp" data-wow-delay="1.5s">
+          <div className="card">
+            <div className="card-header pb-0 border-0 flex-wrap">
+              <div>
+                <div className="mb-3">
+                  <h2 className="heading mb-0" style={{ color: "green" }}>
+                    {t("attended")}
+                  </h2>
+                </div>
+              </div>
+            </div>
+            <div className="table-responsive full-data">
+              <div
+                id="example-student_wrapper"
+                className="dataTables_wrapper no-footer"
+              >
+                <table
+                  className="table-responsive-lg table display dataTablesCard student-tab dataTable no-footer"
+                  id="example-student"
+                >
+                  <thead>
+                    <tr>
+                      <th>{t("image")}</th>
+                      <th>{t("name")}</th>
+                      <th>{t("phoneNumber")}</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {students?.result?.map(
+                      (item, ind) =>
+                        item?.attended && (
+                          <tr key={ind}>
+                            <td>
+                              <img
+                                src={`${settings.baseURL}/images?filename=${item.mainImageName}`}
+                                alt=""
+                                style={{ width: "50px", borderRadius: "8px" }}
+                              />
+                            </td>
+                            <td>
+                              <div className="trans-list">
+                                <h4>{`${item.firstName} ${item.lastName}`}</h4>
+                              </div>
+                            </td>
+                            <td>
+                              <h6 className="mb-0">{item.phoneNumber}</h6>
+                            </td>
+                          </tr>
+                        )
+                    )}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
