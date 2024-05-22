1991796a51122ab5b0be8891ba51c393089f61d6
sharipovme <sharipovsdev@gmail.com>
2024-05-22 17:49:18 +0500

    deploy: main image change

--- src/api/index.js
+++ src/api/index.js
@@ -142,6 +142,13 @@ export const getGovernment = (params) => {
   return request({ method: "GET", url: "/governments", params });
 };
 
+export const changeMainImage = (id, imageName) => {
+  return request({
+    method: "PUT",
+    url: `/students/${id}/photo/main?mainPhotoName=${imageName}`,
+  });
+};
+
 export const getFaces = (params) => {
   return request({
     method: "GET",
