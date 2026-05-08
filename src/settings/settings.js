7edd3680a616b40d2d8407e57212f525db156ea8
Sardorious <sardor.tukhtaev@gmail.com>
2026-05-08 20:43:59 +0500

    fix: handle 'undefined' baseURL in settings.js

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,5 +1,5 @@
 const settings = {
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: (process.env.REACT_APP_BASE_URL && process.env.REACT_APP_BASE_URL !== 'undefined') ? process.env.REACT_APP_BASE_URL : "",
   requestTimeout: 60000 * 5,
   rowsPerPage: 10,
   staleTime: 1000 * 60 * 2,
